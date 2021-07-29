1ae54a160a0231e89fe82c690e066c552160aebd
Jennifer Tram Su <59851267+jennifertramsu@users.noreply.github.com>
2021-07-29 03:03:45 +0000

    rename

--- main.py
+++ main.py
@@ -18,129 +18,108 @@ async def on_ready():
 #commands
 
 @bot.command(name = "teehee", help = 'Responds with a random kaomoji')
-
 async def kaomoji_random(ctx):
     response = random.choice(random.choice(kaomoji_list))
     await ctx.send(response)
     
 @bot.command(name = 'uwu', help = 'Responds with a random joy kaomoji')
-
 async def kaomoji_random(ctx):
-    response = random.choice(kaomoji_joy)
+    response = random.choice(kaomoji_list['joy'])
     await ctx.send(response)
     
 @bot.command(name = 'doki', help = 'Responds with a random love kaomoji')
-
 async def kaomoji_random(ctx):
-    response = random.choice(kaomoji_love)
+    response = random.choice(kaomoji_list['love'])
     await ctx.send(response)
     
 @bot.command(name = 'yamete', help = 'Responds with a random embarrassment kaomoji')
-
 async def kaomoji_random(ctx):
-    response = random.choice(kaomoji_embarrassment)
+    response = random.choice(kaomoji_list['embarrassment'])
     await ctx.send(response)
     
 @bot.command(name = 'F', help = 'Responds with a random sympathy kaomoji')
-
 async def kaomoji_random(ctx):
-    response = random.choice(kaomoji_sympathy)
+    response = random.choice(kaomoji_list['sympathy'])
     await ctx.send(response)
     
 @bot.command(name = 'tsk', help = 'Responds with a random dissatisfaction kaomoji')
-
 async def kaomoji_random(ctx):
-    response = random.choice(kaomoji_dissatisfaction)
+    response = random.choice(kaomoji_list['dissatisfaction'])
     await ctx.send(response)
     
 @bot.command(name = 'YAH', help = 'Responds with a random anger kaomoji')
-
 async def kaomoji_random(ctx):
-    response = random.choice(kaomoji_anger)
+    response = random.choice(kaomoji_list['anger'])
     await ctx.send(response)
     
 @bot.command(name = 'ono', help = 'Responds with a random sad kaomoji')
-
 async def kaomoji_random(ctx):
-    response = random.choice(kaomoji_sad)
+    response = random.choice(kaomoji_list['sad'])
     await ctx.send(response)
     
 @bot.command(name = 'uhu', help = 'Responds with a random pain kaomoji')
-
 async def kaomoji_random(ctx):
-    response = random.choice(kaomoji_pain)
+    response = random.choice(kaomoji_list['pain'])
     await ctx.send(response)
 
 @bot.command(name = 'OWO!', help = 'Responds with a random fear kaomoji')
-
 async def kaomoji_random(ctx):
-    response = random.choice(kaomoji_fear)
+    response = random.choice(kaomoji_list['fear'])
     await ctx.send(response)
     
 @bot.command(name = 'ehh', help = 'Responds with a random indifference kaomoji')
-
 async def kaomoji_random(ctx):
-    response = random.choice(kaomoji_indifference)
+    response = random.choice(kaomoji_list['indifference'])
     await ctx.send(response)
     
 @bot.command(name = 'owo?', help = 'Responds with a random confusion kaomoji')
-
 async def kaomoji_random(ctx):
-    response = random.choice(kaomoji_confusion)
+    response = random.choice(kaomoji_list['confusion'])
     await ctx.send(response)
     
 @bot.command(name = 'ehh?', help = 'Responds with a random doubt kaomoji')
-
 async def kaomoji_random(ctx):
-    response = random.choice(kaomoji_doubt)
+    response = random.choice(kaomoji_list['doubt'])
     await ctx.send(response)
     
 @bot.command(name = 'owo!', help = 'Responds with a random surprise kaomoji')
-
 async def kaomoji_random(ctx):
-    response = random.choice(kaomoji_surprise)
+    response = random.choice(kaomoji_list['surprise'])
     await ctx.send(response)
     
 @bot.command(name = 'hug', help = 'Responds with a random hugging kaomoji')
-
 async def kaomoji_random(ctx):
-    response = random.choice(kaomoji_hugging)
+    response = random.choice(kaomoji_list['hugging'])
     await ctx.send(response)
     
 @bot.command(name = 'wink', help = 'Responds with a random winking kaomoji')
-
 async def kaomoji_random(ctx):
-    response = random.choice(kaomoji_winking)
+    response = random.choice(kaomoji_list['winking'])
     await ctx.send(response)
     
 @bot.command(name = 'gomen', help = 'Responds with a random sorry kaomoji')
-
 async def kaomoji_random(ctx):
-    response = random.choice(kaomoji_sorry)
+    response = random.choice(kaomoji_list['sorry'])
     await ctx.send(response)
     
 @bot.command(name = 'yawn', help = 'Responds with a random sleeping kaomoji')
-
 async def kaomoji_random(ctx):
-    response = random.choice(kaomoji_sleeping)
+    response = random.choice(kaomoji_list['sleeping'])
     await ctx.send(response)
     
 @bot.command(name = 'hehe', help = 'Responds with a random special kaomoji')
-
 async def kaomoji_random(ctx):
-    response = random.choice(kaomoji_special)
+    response = random.choice(kaomoji_list['special'])
     await ctx.send(response)
     
 @bot.command(name = 'friend!', help = 'Responds with a random friend kaomoji')
-
 async def kaomoji_random(ctx):
-    response = random.choice(kaomoji_friends)
+    response = random.choice(kaomoji_list['friends'])
     await ctx.send(response)
 
 @bot.command(name = 'oy', help = 'Responds with a random enemies kaomoji')
-
 async def kaomoji_random(ctx):
-    response = random.choice(kaomoji_enemies)
+    response = random.choice(kaomoji_list['enemies'])
     await ctx.send(response)
 
 TOKEN=os.getenv('TOKEN')
